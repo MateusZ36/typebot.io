2f3ef7675a322f9e5318fd4c9cd9a675170b0970
Mateus Zanatta <mateuszanatta1@gmail.com>
2024-04-30 16:15:30 -0300

    Chains fetching from baseUrl

--- packages/forge/blocks/langserve/actions/invoke.ts
+++ packages/forge/blocks/langserve/actions/invoke.ts
@@ -1,9 +1,6 @@
 import { createAction, option } from '@typebot.io/forge'
 import { baseOptions } from '../baseOptions'
-import { isDefined, sendRequest } from '@typebot.io/lib'
 import axios from 'axios'
-import { z } from '@typebot.io/forge/zod'
-import objectPath from 'object-path'
 
 export const invoke = createAction({
   name: 'Invoke',
@@ -39,7 +36,25 @@ export const invoke = createAction({
       'id': 'fetchChains',
       dependencies: ['baseUrl'],
       fetch: async ({ options }) => {
-        return ['1', '2', '3', '4']
+        const url = `${options.baseUrl}/openapi.json`
+
+        function getInvokeChains(data: any): string[] {
+          let firstSegments = new Set<string>()
+          Object.keys(data).forEach(key => {
+            const parts = key.split('/').filter(part => part) // Removes any empty strings from split
+            if (parts[1] === 'invoke') {
+              firstSegments.add(parts[0]) // Add the first segment to the Set
+            }
+          })
+          return Array.from(firstSegments) // Convert Set to Array to return unique first segments
+        }
+
+        const response = await axios.get(url)
+        console.log('='.repeat(50))
+        console.log(url)
+        console.log('='.repeat(50))
+
+        return getInvokeChains(response.data.paths)
       },
     },
   ],
